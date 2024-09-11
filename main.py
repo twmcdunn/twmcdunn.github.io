f17694058e9e89f098c43f186c4aa1b268c940b3
Timothy W McDunn <130157765+twmcdunn@users.noreply.github.com>
2024-09-10 19:32:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import pyautogui
 from pyscript import document
 from pyscript import display
 
 display("HELLO FROM MAIN")
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        display(positionStr, end='')
+        display('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    display('\n')
     
