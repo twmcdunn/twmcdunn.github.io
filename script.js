fa722a3dd641eb65cbce63d02712b6bb1e0195ad
twmcdunn <twcdunn@gmail.com>
2025-05-22 17:01:10 -0500

    building out forum  Addforum needs to be implemented DB side. forum form needs to be implemented GUI side

--- script.js
+++ script.js
@@ -75,6 +75,70 @@ var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
     });
 }
 
+/*
+add forum needs to be implemented
+function addForum(link, title, author, callback) {
+
+    var params = {
+        ExpressionAttributeValues: {
+            ":increment": { N: "1" }
+        },
+        Key: {
+            ELEM: {
+                S: "CURRENT_LINK_NUM"
+            },
+            ID: {
+                N: "0"
+            }
+        },
+        UpdateExpression: "SET CURRENT_NUM = CURRENT_NUM - :increment",
+        TableName: "DCLP",
+        ReturnValues: "UPDATED_OLD"
+    };
+     ddb.updateItem(params, function (err, data) {
+        if (err) {
+            console.log("error", err);
+        }
+        else {
+            //console.log("CURRENT_EVENT_NUM UPDATED. OLD = ", data.Attributes.CURRENT_NUM.N);
+            //console.log(data.Attributes);
+            var linkNum = Number(data.Attributes.CURRENT_NUM.N) - 1;
+            var params = {
+                Item: {
+                    ELEM: {
+                        S: "LINK"
+                    },
+                    ID: {
+                        N: linkNum + ""
+                    },
+                    COMP_URL: {
+                        S: link + ""
+                    },
+                    TITLE: {
+                        S: title + ""
+                    },
+                    AUTHOR: {
+                        S: author + ""
+                    },
+                    CURRENT_COMMENT_NUM: {
+                        N: "0"
+                    }
+                },
+                TableName: "DCLP"
+            };
+             ddb.putItem(params, function (err, data) {
+                if (err) {
+                    console.log("error", err);
+                }
+                else{
+                    callback();
+                }
+            });
+        }
+    });
+}
+    */
+
 function postComment(compositionID, commentorName, comment, callback) {
     var params = {
         ExpressionAttributeValues: {
@@ -157,6 +221,66 @@ function getLinks(callback) {
     })
 }
 
+function postForumComment(forumID, commentorID, comment, callback) {
+    var params = {
+        ExpressionAttributeValues: {
+            ":increment": { N: "1" }
+        },
+        Key: {
+            ELEM: {
+                S: "FORUM_ELEM"
+            },
+            ID: {
+                N: forumID + ""
+            }
+        },
+        UpdateExpression: "SET CURRENT_COMMENT_NUM = CURRENT_COMMENT_NUM + :increment",
+        TableName: "DCLP",
+        ReturnValues: "UPDATED_OLD"
+    };
+    ddb.updateItem(params, function (err, data) {
+        if (err) {
+            console.log("error", err);
+        }
+        else {
+            var commentNum = Number(data.Attributes.CURRENT_COMMENT_NUM.N) + 1;
+
+            var params = {
+                Item: {
+                    ELEM: {
+                        S: "FORUM_COMMENT_ELEM"
+                    },
+                    ID: {
+                        N: ((Number(forumID) * 1000000) - commentNum) + ""
+                    },
+                    COMMENT_CONTENT: {
+                        S: comment + ""
+                    },
+                    COMMENTOR_ID: {
+                        S: commentorID + ""
+                    },
+                    COMMENT_NUM: {
+                        N: commentNum + ""
+                    },
+                    POST_DATE: {
+                        N: Date.now() + ""
+                    }
+                },
+                TableName: "DCLP"
+            };
+            ddb.putItem(params, function (err, data) {
+                if (err) {
+                    console.log("error", err);
+                }
+                else {
+                    callback();
+                }
+            });
+
+        }
+    });
+}
+
 function getComments(compositionID, callback) {
     var params = {
         KeyConditionExpression: "ELEM = :val AND ID BETWEEN :compidLower AND :compid",
@@ -166,6 +290,29 @@ function getComments(compositionID, callback) {
             ":compidLower": { N: ((Number(compositionID) - 1) * 1000000) + "" },
         },
         ProjectionExpression: "COMMENTOR_NAME, COMMENT_CONTENT, POST_DATE",
+        TableName: "DCLP"
+    };
+
+    ddb.query(params, function (err, data) {
+        if (err) {
+            console.log("error", err);
+        } else {
+            console.log("DATA", data);
+            //data.Items.sort((a,b) => Number(a.COMMENT_NUM.N) - Number(b.COMMENT_NUM.N))
+            callback(data.Items);
+        }
+    })
+}
+
+function getForumComments(forumID, callback) {
+    var params = {
+        KeyConditionExpression: "ELEM = :val AND ID BETWEEN :forumidLower AND :forumid",
+        ExpressionAttributeValues: {
+            ":val": { S: "FORUM_COMMENT_ELEM" },
+            ":formid": { N: (Number(forumID) * 1000000) + "" },
+            ":forumidLower": { N: ((Number(forumID) - 1) * 1000000) + "" },
+        },
+        ProjectionExpression: "COMMENTOR_ID, COMMENT_CONTENT, POST_DATE",
         TableName: "DCLP"
     };
 
